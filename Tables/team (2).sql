f6f4c3a94cdcce2b0cec94b42d005e89f7ae1dc5
shobhit-coder <shobhit1998@yahoo.co.in>
2018-11-03 22:47:24 +0530

    initiall commit

---
+++ Tables/team (2).sql
@@ -0,0 +1,55 @@
+CREATE TABLE teams(
+   Sl        INTEGER  NOT NULL
+  ,TID       INTEGER  NOT NULL PRIMARY KEY 
+  ,Team_Name VARCHAR(24) NOT NULL
+  ,ShortName VARCHAR(14) NOT NULL
+);
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (1,1,'Arsenal','Arsenal');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (2,2,'Aston Villa','Aston Villa');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (3,3,'Blackburn Rovers','Blackburn');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (4,4,'Chelsea','Chelsea');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (5,5,'Coventry City','Coventry');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (6,6,'Crystal Palace','Crystal Palace');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (7,7,'Everton','Everton');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (8,8,'Ipswich Town','Ipswich');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (9,9,'Leeds United','Leeds');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (10,10,'Liverpool','Liverpool');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (11,11,'Manchester City','Man City');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (12,12,'Manchester United','Man Utd');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (13,13,'Middlesbrough','Middlesbrough');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (14,14,'Norwich City','Norwich');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (15,15,'Nottingham Forest','Nott''m Forest');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (16,16,'Oldham Athletic','Oldham');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (17,17,'Queens Park Rangers','QPR');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (18,18,'Sheffield United','Sheffield Utd');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (19,19,'Sheffield Wednesday','Sheffield Wed');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (20,20,'Southampton','Southampton');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (21,21,'Tottenham Hotspur','Spurs');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (22,22,'Wimbledon','Wimbledon');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (23,23,'Newcastle United','Newcastle');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (24,24,'Swindon Town','Swindon');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (25,25,'West Ham United','West Ham');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (26,26,'Leicester City','Leicester');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (27,27,'Bolton Wanderers','Bolton');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (28,28,'Derby County','Derby');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (29,29,'Sunderland','Sunderland');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (30,30,'Barnsley','Barnsley');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (31,31,'Charlton Athletic','Charlton');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (32,32,'Bradford City','Bradford');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (33,33,'Watford','Watford');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (34,34,'Fulham','Fulham');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (35,35,'Birmingham City','Birmingham');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (36,36,'West Bromwich Albion','West Brom');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (37,37,'Portsmouth','Portsmouth');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (38,38,'Wolverhampton Wanderers','Wolves');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (39,39,'Wigan Athletic','Wigan');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (40,40,'Reading','Reading');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (41,41,'Hull City','Hull');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (42,42,'Stoke City','Stoke');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (43,43,'Burnley','Burnley');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (44,44,'Blackpool','Blackpool');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (45,45,'Swansea City','Swansea');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (46,46,'Cardiff City','Cardiff');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (47,127,'Bournemouth','Bournemouth');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (48,131,'Brighton and Hove Albion','Brighton');
+INSERT INTO teams(Sl,TID,Team_Name,ShortName) VALUES (49,159,'Huddersfield Town','Huddersfield');
---
+++ login.py
@@ -0,0 +1,72 @@
+from flask import Flask,redirect,url_for,request,render_template
+app = Flask(__name__)
+import mysql.connector
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+# @app.route('/hello/<hello>')
+# def helloPage(hello):
+#     return 'hahaha'+str(hello)
+
+# @app.route('/h1/')
+# def h1():
+#     return redirect(url_for('helloPage',hello='kkk'))
+
+# if __name__ == '__main__':
+#    app.run()
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="battlefield4",
+  database="footballmanagement"
+)
+print(mydb)
+session=dict()
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['username']=request.form['username']
+            session['password']=request.form['password']
+            return redirect(url_for('home'))
+    return render_template('index.html', error=error)
+
+@app.route('/home')
+def home():
+    return "username is"+str(session['username'])+"password is"+str(session['password'])
+
+# @app.route('/')
+# def start():
+#     return render_template('index.html')
+
+# @app.route('/fan')
+# def fan():
+#     tid = request.args.get('tid')
+#     user = request.args.get('user')
+#     print('welcome'+ tid)
+#     return 'welcome %s !!!! teamid %s' % user % tid
+
+# @app.route('/login',methods = ['POST', 'GET'])
+# def login():
+#    if request.method == 'POST':
+#       user = request.form['username']
+#       mycursor = mydb.cursor()
+#       mycursor.execute("SELECT tid FROM user where '"+user+"'=usn;")
+#       myresult = mycursor.fetchall()
+#       print(myresult[0][0])
+#       return redirect(url_for('fan',tid=str(myresult[0][0]),user=user))
+#    else:
+#       user = request.args.get('username')
+#       mycursor = mydb.cursor()
+#       mycursor.execute("SELECT tid FROM user where '"+user+"'=usn;")
+#       myresult = mycursor.fetchall()
+#       print(myresult[0][0])
+#       return redirect(url_for('fan',tid=str(myresult[0][0]),user=user))
+
+if __name__ == '__main__':
+   app.run(debug = True)
